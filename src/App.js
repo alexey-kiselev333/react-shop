0e71115c632166d80ce86d892c8310c3d720b4e8
alexey-kiselev333 <Kiselev.Alexey.Sergeevich1@yandex.ru>
2021-08-19 11:58:07 +0300

    create_Drawer

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
 import logo from './logo.svg';
 import './App.scss';
+import Card from "./Components/Card";
+import Header from "./Components/Header";
+import Drawer from "./Components/Drawer";
 
 function App() {
     return (
         <div className="wrapper clear">
-            <header>
-                <div className="headerLeft">
-                    <img width={40} height={40  } src="/img/logo.png"/>
-                    <div className="headerInfo">
-                        <h3 className="text-up">React Sneakers</h3>
-                        <p className="opacity">Магазин лучших кроссовок</p>
-                    </div>
-                </div>
-                <ul className="headerRight">
-                    <li className="distance">
-                        <img width={18} height={18} src="/img/cart.svg"/>
-                        <span>1285 руб</span>
-                    </li>
-                    <li>
-                        <img width={18} height={18} src="/img/user.svg"/>
-                    </li>
-                </ul>
-            </header>
+
+                <Drawer/>
+
+            <Header/>
             <div className="content distance-pad">
+                <div className="d-flex align-center mb-40 justify-between ">
                 <h1>Все кроссовки</h1>
+                <div className="search-block d-flex">
+                    <img src="/img/search.svg" alt="Search"/>
+                    <input placeholder="поиск..."/>
+                </div>
+                </div>
                 <div className="stroka">
+                <Card/>
                 <div className="card">
-                    <img width={133} height={112} src="/img/sneakers/1.jpg"/>
+                    <img width={133} height={112} src="/img/sneakers/2.jpg" alt="2sn"/>
                     <div className="di">
                         <p>Мужские Кроссовки</p>
                         <p>Nike Blazer Mid Suede</p>
@@ -37,12 +33,12 @@ function App() {
                             <b>12 999 руб.</b>
                         </div>
                         <button className="button">
-                            <img width={11} height={11} src="/img/plus.svg"/>
+                            <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
                         </button>
                     </div>
                 </div>
                 <div className="card">
-                    <img width={133} height={112} src="/img/sneakers/2.jpg"/>
+                    <img width={133} height={112} src="/img/sneakers/3.jpg" alt="3sn"/>
                     <div className="di">
                         <p>Мужские Кроссовки</p>
                         <p>Nike Blazer Mid Suede</p>
@@ -53,12 +49,12 @@ function App() {
                             <b>12 999 руб.</b>
                         </div>
                         <button className="button">
-                            <img width={11} height={11} src="/img/plus.svg"/>
+                            <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
                         </button>
                     </div>
                 </div>
                 <div className="card">
-                    <img width={133} height={112} src="/img/sneakers/3.jpg"/>
+                    <img width={133} height={112} src="/img/sneakers/4.jpg" alt="4sn"/>
                     <div className="di">
                         <p>Мужские Кроссовки</p>
                         <p>Nike Blazer Mid Suede</p>
@@ -69,23 +65,7 @@ function App() {
                             <b>12 999 руб.</b>
                         </div>
                         <button className="button">
-                            <img width={11} height={11} src="/img/plus.svg"/>
-                        </button>
-                    </div>
-                </div>
-                <div className="card">
-                    <img width={133} height={112} src="/img/sneakers/4.jpg"/>
-                    <div className="di">
-                        <p>Мужские Кроссовки</p>
-                        <p>Nike Blazer Mid Suede</p>
-                    </div>
-                    <div className="card-bottom">
-                        <div className="card-bottom-plus">
-                            <span>Цена:</span>
-                            <b>12 999 руб.</b>
-                        </div>
-                        <button className="button">
-                            <img width={11} height={11} src="/img/plus.svg"/>
+                            <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
                         </button>
                     </div>
                 </div>
---
+++ src/Components/Header.jsx
@@ -0,0 +1,22 @@
+function Header(){
+    return <header>
+        <div className="headerLeft">
+            <img width={40} height={40  } src="/img/logo.png" alt="Logo"/>
+            <div className="headerInfo">
+                <h3 className="text-up">React Sneakers</h3>
+                <p className="opacity">Магазин лучших кроссовок</p>
+            </div>
+        </div>
+        <ul className="headerRight">
+            <li className="distance">
+                <img width={18} height={18} src="/img/cart.svg" alt="Cart"/>
+                <span>1285 руб</span>
+            </li>
+            <li>
+                <img width={18} height={18} src="/img/user.svg" alt="user"/>
+            </li>
+        </ul>
+    </header>
+}
+
+export default Header
